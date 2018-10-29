fa524b819420473d5e45bd74efaa267e5c97a572
Yash Mittal <yashmittal2009@gmail.com>
2018-10-29 16:05:22 -0400

    Tabs to spaces in app.js

--- src/server/app.js
+++ src/server/app.js
@@ -7,20 +7,20 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/translate', (req, res) => {
-	asciiMarkups = req.body.markups;
-	if (asciiMarkups === undefined) {
-		asciiMarkups = Array();
-	} else if (asciiMarkups.constructor !== Array) {
-		asciiMarkups = [asciiMarkups];
-	}
+    asciiMarkups = req.body.markups;
+    if (asciiMarkups === undefined) {
+        asciiMarkups = Array();
+    } else if (asciiMarkups.constructor !== Array) {
+        asciiMarkups = [asciiMarkups];
+    }
 
-	latexMarkups = asciiMarkups.map((asciiMarkup) => {
-		return ASCIIMathTeXImg.AMTparseAMtoTeX(asciiMarkup);
-	});
-	res.send(latexMarkups);
+    latexMarkups = asciiMarkups.map((asciiMarkup) => {
+        return ASCIIMathTeXImg.AMTparseAMtoTeX(asciiMarkup);
+    });
+    res.send(latexMarkups);
 });
 
 const port = 8080;
 app.listen(port, () => {
-	console.log('App listening on port', port);
+    console.log('App listening on port', port);
 });
