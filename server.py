368f6492bdfbb31446ff439fdbafb9a6c0eac2f8
Yash Mittal <yashmittal2009@gmail.com>
2019-03-09 15:05:28 -0500

    Update host/port

--- server.py
+++ server.py
@@ -36,5 +36,5 @@ def convert():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
